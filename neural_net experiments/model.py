b0d2b210897396a34a01f86c053c27b212fd04f2
mayar-ahmed <mayar.ahefny@gmail.com>
2017-12-13 00:13:28 +0200

    last version

--- neural_net experiments/model.py
+++ neural_net experiments/model.py
@@ -10,7 +10,7 @@ import os
 import numpy as np
 
 expno=12
-reg=0.05
+
 directory="model1/experiments/exp{}/checkpoints/".format(expno)
 if not os.path.exists(directory):
     os.makedirs(directory)
@@ -28,38 +28,38 @@ y_val=np.load("../data/y_val.npy")
 model = Sequential()
 
 #kernal initialier is uniform
-model.add(Conv2D(32,(5,5), strides=2,input_shape=(256, 256,1), kernel_regularizer=regularizers.l2(reg)))
+model.add(Conv2D(32,(5,5), strides=2,input_shape=(256, 256,1)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 
-model.add(Conv2D(32,(3,3) ,strides=1,kernel_regularizer=regularizers.l2(reg)))
+model.add(Conv2D(32,(3,3) ,strides=1))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 
-model.add(Conv2D(32,(3,3) ,strides=1,kernel_regularizer=regularizers.l2(reg) ))
+model.add(Conv2D(32,(3,3) ,strides=1 ))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2),))
 
-model.add(Conv2D(32,(3,3) ,strides=1,kernel_regularizer=regularizers.l2(reg)))
+model.add(Conv2D(32,(3,3) ,strides=1))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 
 
 model.add(Flatten())
-model.add(Dense(128,kernel_regularizer=regularizers.l2(reg)))
+model.add(Dense(128))
 model.add(Activation('relu'))
 model.add(Dropout(0.1))
 
-model.add(Dense(64,kernel_regularizer=regularizers.l2(reg)))
+model.add(Dense(64))
 model.add(Activation('relu'))
 model.add(Dropout(0.1))
 
 
-model.add(Dense(32,kernel_regularizer=regularizers.l2(reg)))
+model.add(Dense(32))
 model.add(Activation('relu'))
 model.add(Dropout(0.1))
 
-model.add(Dense(3,kernel_regularizer=regularizers.l2(reg)))
+model.add(Dense(3))
 model.add(Activation('softmax'))
 
 #checkpoint
@@ -69,7 +69,7 @@ filepath="model1/experiments/exp{}/checkpoints/weights-best-{{val_acc:.2f}}.hdf5
 checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=False, mode='max')
 tensorboard=TensorBoard(log_dir="model1/experiments/exp{}/summaries".format(expno))
 
-adam=RMSprop(lr=1e-3)
+rms=RMSprop(lr=1e-3)
 
-model.compile(optimizer=adam,loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+model.compile(optimizer=rms,loss='sparse_categorical_crossentropy',metrics=['accuracy'])
 model.fit(x_train,y_train,validation_data=(x_val,y_val), shuffle=True, batch_size=32,callbacks=[tensorboard,checkpoint],epochs=20)
